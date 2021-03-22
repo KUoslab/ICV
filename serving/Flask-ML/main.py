e5b8a1f3315af03a60bdc62a201152f7cbf4df72
Hyowon Lee <hyowonlee@Hyowonui-MacBookPro.local>
2021-03-22 11:51:50 +0900

    .

--- serving/Flask-ML/main.py
+++ serving/Flask-ML/main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import logging
 import flask
 from flask import Flask, request, render_template, jsonify
 from sklearn.externals import joblib
@@ -11,12 +10,6 @@ from serve import get_model_api
 
 app = Flask(__name__)
 
-# logging for heroku
-if 'DYNO' in os.environ:
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.INFO)
-
-# load the model
 model_api = get_model_api(model)
 
 @app.route("/")
@@ -27,16 +20,13 @@ def index():
 @app.route('/predict', methods=['POST'])
 def make_prediction():
     if request.method == 'POST':
+        # 입력 예외 처리
         input_data = request.json
         output_data = model_api(input_data)
         response = jsonify(output_data)
+        # output_data >> cgroup
         return response
 
-
-
 if __name__ == '__main__':
-    # 모델 로드
-    # ml/model.py 선 실행 후 생성
     model = joblib.load('./model/model.pkl')
-    # Flask 서비스 스타트
     app.run(host='0.0.0.0', port=8000, debug=True)
