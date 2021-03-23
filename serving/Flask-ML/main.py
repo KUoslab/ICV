5f664f3176c0232625637a318ebe8e50741a7e1f
Hyowon Lee <hyowonlee@Hyowonui-MacBookPro.local>
2021-03-23 11:18:25 +0900

    serving/Flask-ML/inference engine

--- serving/Flask-ML/main.py
+++ serving/Flask-ML/main.py
@@ -1,19 +1,19 @@
 import os
 import sys
-import flask
-from flask import Flask, request, render_template, jsonify
+# import flask
+# from flask import Flask, request, render_template, jsonify
 import joblib
 import numpy as np
 import pandas as pd
 import json
-from pandas import json_normalize
-from scipy import misc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 from serve import get_model_api
 
 app = Flask(__name__)
 
-model_api = get_model_api(model)
+model_api = get_model_api()
 
 @app.route("/")
 @app.route("/index")
@@ -36,12 +36,23 @@ def make_prediction():
         }
         '''
 
-        output_data = model_api(input_data)
+        output_data = model_api(model, min_max_scalar, input_data)
 
         # response = jsonify(output_data)
         # output_data >> cgroup
         return response
 
+def MinMaxScaler():
+    dataset = pd.read_csv('./data/cpu_quota.csv', names=['thread_quota', 'packet_size', 'bandwidth_tx', 'pps_tx', 'cpu_usage'])
+    y = np.array(dataset['thread_quota'])
+    X = np.array(dataset.drop('thread_quota', axis=1))
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.20, random_state=40)
+    min_max_scalar = MinMaxScaler()
+    train_X_ppr = min_max_scalar.fit_transform(train_X)
+    train_y_ppr = min_max_scalar.fit_transform(train_y.reshape(-1, 1))
+    return min_max_scalar
+
 if __name__ == '__main__':
     model = joblib.load('./model/test')
+    min_max_scaler = MinMaxScaler()
     app.run(host='0.0.0.0', port=8000, debug=True)
--- serving/Flask-ML/serve.py
+++ serving/Flask-ML/serve.py
@@ -11,19 +11,26 @@ def calculate_pps(df):
     pps = 50000
     return pps
 
+def data_preprocessing(df, min_max_scalar):
+    df.loc['pps_tx'] = [calculate_pps(df)]
+    df.loc['cpu_usage'] = [60.0]
+
+    arr = np.array(df)
+    input_data = min_max_scalar.transform(arr)
+    return input_data
+
+def data_postprocessing(arr, min_max_scalar):
+    output_data = min_max_scalar.inverse_transform(preds.reshape(-1, 1))
+    return output_data[0][0]
+
 def print_df(df):
     print(tabulate(df, headers='keys', tablefmt='psql')) 
 
 def get_model_api():
-    model = sys.argv[1]
-
-    def model_api(df):
-        df.loc['pps_tx'] = [calculate_pps(df)]
-        df.loc['cpu_usage'] = [60.0]
-        print_df(df)
-
-        preds = model.predict(df)
-        print(preds)
-        return preds
+    def model_api(model, min_max_scaler, df):
+        input_data = data_preprocessing(df, min_max_scalar)
+        preds = model.predict(input_data)
+        output_data = data_postprocessing(preds, min_max_scalar)
+        return output_data
 
     return model_api
