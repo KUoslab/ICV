d0e3e5fa51ca4b34f167864abdcd31a723d4cf67
Leehyowon <hyowon6724@naver.com>
2021-03-27 20:35:53 +0900

    experiment 1

--- serving/experiments/serving_1.sh
+++ serving/experiments/serving_1.sh
@@ -10,6 +10,7 @@ do
 
     cd /sys/fs/cgroup/cpu/machine/qemu-$VM.libvirt-qemu/emulator
     sudo cgset -r cpu.cfs_quota_us=$quota machine/qemu-$VM.libvirt-qemu/emulator
+    # sudo cgset -r cpu.cfs_quota_us=100000 machine/qemu-1-test.libvirt-qemu/emulator
 
     sshpass -p'1' ssh -oStrictHostKeyChecking=no storage@163.152.20.144 "netperf -H 163.152.20.212 -l 120 -- -m 1024" > $PATH/data/output_full.txt
 
