15a8e1277e0a1e9d034dcc9f29d95504744424c1
Hyowon Lee <hyowonlee@Hyowonui-MacBookPro.local>
2021-03-30 13:44:39 +0900

    .

--- serving/experiments/test.sh
+++ serving/experiments/test.sh
@@ -8,7 +8,7 @@ python3 $EXPATH/code/serving_$model_name.py
 while IFS=',' read pkt_size slo pps cpu quota
 do
     cd $CGPATH
-    sudo cgset -r cpu.cfs_quota_us='expr "$quota"' machine/qemu-$VM.libvirt-qemu/emulator
+    sudo cgset -r cpu.cfs_quota_us=${quota%.*} machine/qemu-$VM.libvirt-qemu/emulator
     sshpass -p'1' ssh -oStrictHostKeyChecking=no storage@163.152.20.144 "netperf -H 163.152.20.212 -l 1 -- -m $pkt_size" > $EXPATH/data/output_full.txt
     cd $EXPATH
     python3 code/output.py "$model_name" "$slo"
