2f14bff7f2ee69f805193a5133b1e9e916b5e3e4
Leehyowon <hyowon6724@naver.com>
2021-04-07 11:13:59 +0900

    .

--- serving/experiments/code/serving_test.py
+++ serving/experiments/code/serving_test.py
@@ -19,9 +19,40 @@ var_cpu_usage = 100.18
 
 model_name = "random_forest"
 var_packet_size = 64
+var_cpu_usage = 70.00
+
+# 1
 var_bandwidth_tx = 311.01
 var_pps_tx = 27030
-var_cpu_usage = 71.88
+# 2
+# var_bandwidth_tx = 346.96
+# var_pps_tx = 29966
+# 3
+# var_bandwidth_tx = 389.19
+# var_pps_tx = 35239
+# 4
+# var_bandwidth_tx = 369.82
+# var_pps_tx = 39039
+# 5
+# var_bandwidth_tx = 405.91
+# var_pps_tx = 40025
+
+# var_packet_size = 1024
+# 1
+# var_bandwidth_tx = 409.24
+# var_pps_tx = 34650
+# 2
+# var_bandwidth_tx = 964.53
+# var_pps_tx = 81827
+# 3
+# var_bandwidth_tx = 1240
+# var_pps_tx = 105102
+# 4
+# var_bandwidth_tx = 2510
+# var_pps_tx = 213229
+# 5
+# var_bandwidth_tx = 2720
+# var_pps_tx = 231082
 
 
 model = joblib.load('./model/' + model_name)
